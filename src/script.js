e5ebbbe7ff280a1ef7f82312c44036a0a1e27ab9
João Muraro <murarorestelattojoaovictor@gmail.com>
2024-10-27 20:40:15 -0300

    Imagens, modelo1 e etc
    Adicionei as imagens a pasta assets, adicionei o modelo 3d de marte para a seção 2 e alterei a posição e propriedades de rotação do planeta da seção 1

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
-    const rotationAngle = scrollPosition / 5; 
+    const rotationAngle = scrollPosition / 20; 
     const image = document.querySelector('.mars');
     
     image.style.transform = `rotate(${Math.min(rotationAngle, 90)}deg)`;
