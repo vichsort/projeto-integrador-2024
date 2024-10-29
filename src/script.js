4070c43ecffd4736c18c70947a29467ed3b5f761
Vitor Mignoni <vichsort@proton.me>
2024-10-28 21:50:41 -0300

    carousel pt I
    primeira tentativa de carrossel.

--- src/script.js
+++ src/script.js
@@ -5,4 +5,18 @@ window.addEventListener('scroll', () => {
     
     image.style.transform = `rotate(${Math.min(rotationAngle, 360)}deg)`;
 });
-  
+
+let indiceSlide = 0;
+
+function mudarSlide(direcao) {
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+  indiceSlide = (indiceSlide + direcao + totalSlides) % totalSlides;
+  const larguraSlide = document.querySelector('.container').offsetWidth;
+  document.querySelector('.carrossel').style.transform = `translateX(${-indiceSlide * larguraSlide}px)`;
+}
+
+window.addEventListener('resize', () => {
+  const larguraSlide = document.querySelector('.container').offsetWidth;
+  document.querySelector('.carrossel').style.transform = `translateX(${-indiceSlide * larguraSlide}px)`;
+});
