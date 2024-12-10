2c10e4fe7c5727763982c9816d56e21917a2c737
Vitor Mignoni <vichsort@proton.me>
2024-12-10 14:56:13 -0300

    correções pós-apresentação
    Primeiras correções compõem:
    - identação simplificada
    - correção do nome de package.json
    - botão com padding defeitusoso
    - comentado o carrossel defeituoso
    - retirados espaços desnecessários

--- src/script.js
+++ src/script.js
@@ -7,34 +7,34 @@ window.addEventListener("scroll", () => {
 });
 
 
-// carrossel
-const carousel = document.querySelector('.carousel');
-const items = document.querySelectorAll('.carousel-item');
-const totalItems = items.length;
-let currentIndex = 1; 
+// // carrossel
+// const carousel = document.querySelector('.carousel');
+// const items = document.querySelectorAll('.carousel-item');
+// const totalItems = items.length;
+// let currentIndex = 1; 
 
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-item');
-    slides.forEach((slide, i) => {
-        slide.classList.remove('active');
-        if (i === index) {
-            slide.classList.add('active');
-        }
-    });
-    document.querySelector('.carousel').style.transform = `translateX(-${index * 50}%)`; // aqui que tem a rolagem
-}
+// function showSlide(index) {
+//     const slides = document.querySelectorAll('.carousel-item');
+//     slides.forEach((slide, i) => {
+//         slide.classList.remove('active');
+//         if (i === index) {
+//             slide.classList.add('active');
+//         }
+//     });
+//     document.querySelector('.carousel').style.transform = `translateX(-${index * 50}%)`; // aqui que tem a rolagem
+// }
 
-document.querySelector('.next').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % totalItems;
-    showSlide(currentIndex);
-});
+// document.querySelector('.next').addEventListener('click', () => {
+//     currentIndex = (currentIndex + 1) % totalItems;
+//     showSlide(currentIndex);
+// });
 
-document.querySelector('.prev').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-    showSlide(currentIndex);
-});
+// document.querySelector('.prev').addEventListener('click', () => {
+//     currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+//     showSlide(currentIndex);
+// });
 
-showSlide(currentIndex);
+// showSlide(currentIndex);
 
 // Player - Seção 7
 
