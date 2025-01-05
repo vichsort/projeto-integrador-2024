d4dce027e9c5c570ac547c76aa363f6ebcc06902
Vitor Mignoni <vichsort@proton.me>
2025-01-05 13:56:09 -0300

    comp carrossel

--- src/script.js
+++ src/script.js
@@ -5,37 +5,6 @@ window.addEventListener("scroll", () => {
 
   image.style.transform = `rotate(${Math.min(rotationAngle, 360)}deg)`;
 });
-
-
-// // carrossel
-// const carousel = document.querySelector('.carousel');
-// const items = document.querySelectorAll('.carousel-item');
-// const totalItems = items.length;
-// let currentIndex = 1; 
-
-// function showSlide(index) {
-//     const slides = document.querySelectorAll('.carousel-item');
-//     slides.forEach((slide, i) => {
-//         slide.classList.remove('active');
-//         if (i === index) {
-//             slide.classList.add('active');
-//         }
-//     });
-//     document.querySelector('.carousel').style.transform = `translateX(-${index * 50}%)`; // aqui que tem a rolagem
-// }
-
-// document.querySelector('.next').addEventListener('click', () => {
-//     currentIndex = (currentIndex + 1) % totalItems;
-//     showSlide(currentIndex);
-// });
-
-// document.querySelector('.prev').addEventListener('click', () => {
-//     currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-//     showSlide(currentIndex);
-// });
-
-// showSlide(currentIndex);
-
 // Player - Seção 7
 
 const audio = document.getElementById('audio');
