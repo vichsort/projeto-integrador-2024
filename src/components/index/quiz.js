35566675d1e0cd809a49a1d1bbdca55b8041c43b
João Muraro <murarorestelattojoaovictor@gmail.com>
2025-01-13 17:04:39 -0300

    Update quiz.js

--- src/components/index/quiz.js
+++ src/components/index/quiz.js
@@ -17,6 +17,7 @@ class QuizComponent extends LitElement {
       border-radius: 10px;
       width: 1000px;
       margin: 0 auto;
+      font-size: 1.5rem;
     }
 
     .question {
@@ -88,14 +89,64 @@ class QuizComponent extends LitElement {
     super();
     this.questions = [
       {
-        question: 'Pergunta 1',
-        answers: ['Resposta 1', 'Resposta 2', 'Resposta 3'],
+        question: 'Quais foram as três sondas mais famosas lançadas pela NASA?',
+        answers: ['Perseverance, Exploration e Mars I', 'Explorer, Curiosity e Perseverance', 'Curiosity, Space IV e Explorer'],
+        correct: 1
+      },
+      {
+        question: 'Em que ano a sonda Perseverance foi lançada?',
+        answers: ['1998', '2020', '2021'],
         correct: 2
       },
       {
-        question: 'Pergunta 2',
-        answers: ['Resposta A', 'Resposta B', 'Resposta C'],
+        question: 'O que é um desafio para a colonização de Marte?',
+        answers: ['A falta de água e oxigênio', 'A falta de tecnologia para lançar satélites', 'A presença de vida extraterrestre'],
+        correct: 0
+      },
+      {
+        question: 'Quais minerais encontrados em Marte são essenciais para a agricultura e sustentabilidade?',
+        answers: ['Ferro e níquel', 'Fósforo e nitrogênio', 'Platina e carbono'],
         correct: 1
+      },
+      {
+        question: 'O que tem impulsionado as discussões sobre a colonização de outros corpos celestes?',
+        answers: ['Superlotação futura da Terra', 'A busca por mais recursos naturais', 'A descoberta de novas tecnologias'],
+        correct: 0
+      },
+      {
+        question: 'Qual é o objetivo das missões de exploração espacial, como a da sonda Mars Curiosity Rover?',
+        answers: ['Estudar a atmosfera da Terra', 'Explorar asteróides próximos', 'Descobrir sinais de vida em Marte'],
+        correct: 2
+      },
+      {
+        question: 'Por que Marte é considerado um dos principais alvos para a colonização humana?',
+        answers: ['Porque é o planeta mais próximo da Terra', 'Por ter uma atmosfera rica em oxigênio', 'Por apresentar características semelhantes a da Terra'],
+        correct: 2
+      },
+      {
+        question: 'Qual é a principal evidência que sugere que Marte já teve água em estado líquido no passado?',
+        answers: ['A presença de minerais raros', 'A descoberta de vales fluviais erodidos', 'A presença de tempestades de areia'],
+        correct: 1
+      },
+      {
+        question: 'Qual foi a descoberta significativa feita pelo Curiosity Rover em 2018?',
+        answers: ['Moléculas orgânicas complexas preservadas em rochas', 'Indícios de um grande oceano subterrâneo', 'Presença de uma magnetosfera natural'],
+        correct: 0
+      },
+      {
+        question: '⁠Por que a robótica é importante nas missões de exploração espacial?',
+        answers: ['Porque são mais baratos do que missões tripuladas', 'Porque os robôs podem sobreviver mais facilmente nas condições extremas do espaço', 'Porque permite a construção de colônias em outros planetas'],
+        correct: 1
+      },
+      {
+        question: 'O que é necessário para transformar Marte em um ambiente habitável para os seres humanos?',
+        answers: ['A criação de uma atmosfera densa e oxigenada', 'A construção de habitats subterrâneos', 'A mudança da órbita de Marte'],
+        correct: 0
+      },
+      {
+        question: 'Qual é o maior obstáculo para a colonização de Marte atualmente?',
+        answers: ['A falta de tecnologias para explorar outros planetas', 'O alto custo das missões espaciais', 'A pressão atmosférica extremamente baixa de Marte'],
+        correct: 2
       }
     ];
     this.currentQuestionIndex = 0;
